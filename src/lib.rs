c5a70501e2641c91d1096ec2eab1db7272f2fd3f
conduition <conduition@proton.me>
2023-10-28 21:07:01 +0000

    fail to compile if no features are enabled

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![doc = include_str!("../README.md")]
 #![doc = include_str!("../doc/API.md")]
 
+#[cfg(all(not(feature = "secp256k1"), not(feature = "k256")))]
+compile_error!("At least one of the `secp256k1` or `k256` features must be enabled.");
+
 mod arithmetic;
 pub mod errors;
 mod points;
